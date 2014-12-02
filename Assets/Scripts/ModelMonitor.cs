89d4f56c234f594efd2e6c0abe29230d2e4638f6
mingchaoyan <mingchaoyan@gmail.com>
2014-12-02 22:08:25 +0800

    添加脚本令模型旋转

---
+++ Assets/Scripts/ModelMonitor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ModelMonitor : MonoBehaviour {
+    private GameObject _model = null;
+
+	// Use this for initialization
+	void Start () {
+        _model = GameObject.Find("Main Camera/Orc");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _model.transform.Rotate(0, 180*Time.deltaTime, 0);
+	}
+}
